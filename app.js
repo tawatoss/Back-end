087c744be6efa10823274c1f4dc566491f42d400
tawatoss <thanatosolimpo267@gmail.com>
2022-10-20 20:50:50 -0300

    kkkk

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-const express = require('express');
-const morgan = require('morgan');
+const bodyParser = require("body-parser");
+const express = require("express");
+
+
+
 const app = express();
-app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 const rotaUsuarios = require('./routes/rotaUsuario');
-app.use("/usuario",rotaUsuarios);
 
-app.use((req,res,next)=>{
-      const erro = new Error("Não encontrado!");
-      erro.status(404);
-      next(erro);
-});
-app.use((error,req,res,next)=>{
-        res.status(error.status || 500);
-        return res.json({
-            erro:{
-                 mensagem:error.message
-            }
-        })
-})
+app.use('/usuario',rotaUsuarios);
 
-module.exports = app
+module.exports = app;
+
--- routes/rotaUsuario.js
+++ routes/rotaUsuario.js
@@ -1,30 +1,93 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-//para consultar todos os dados
-router.get('/',(req,res,next)=>{
-    res.status(200).send({
-        mensagem:"aqui é a lista de usuários!!!!",
-        nome:"carlos"
-      })
-})
-//para consultar um determinado cadastro
-router.get('/:id',(req,res,next)=>{
-    const id = req.params.id;
-    res.status(200).send({
-        mensagem:`aqui é a lista de um usuário com id:${id}`,
-        nome:"carlos"
-      })
-})
-// para enviar dados para salvar no banco
-router.post('/',(req,res,next)=>{
-          const nome = req.body.nome;
-          const email = req.body.email;
-          const senha = req.body.senha;
-          res.status(201).send({
-            
-          })
-})
+
+
+
+const usuarios=[
+   
+    {
+        id:1,
+        nome:"Derick",
+        email:"derick@gmail.com",
+        senha:"123"
+
+    },
+    
+    {
+        id:2,
+        nome:"Iudhy",
+        email:"iudhy@gmail.com",
+        senha:"123"
+
+    },
+
+    {
+        id:3,
+        nome:"Willian",
+        email:"william@gmail.com",
+        senha:"123"
+
+    },
+
+    {
+        id:4,
+        nome:"Bispo",
+        email:"bispo@gmail.com",
+        senha:"123"
+
+    },
+
+    {
+
+        id:5,
+        nome:"Antonio",
+        email:"antonio@gmail.com",
+        senha:"123"
+
+    },
+
+    {
+        id:6,
+        nome:"Raimundo",
+        email:"raimundo@gmail.com",
+        senha:"123"
+
+    },
+
+    {
+
+        id:7,
+        nome:"Wallysson",
+        email:"wallysson@gmail.com",
+        senha:"123"
+
+    },
+
+    { 
+        id:8,
+        nome:"Wesley",
+        email:"wesley@gmail.com",
+        senha:"123"
+
+    },
+
+    {
+        id:9,
+        nome:"Gui",
+        email:"gui@gmail.com",
+        senha:"123"
+
+    },
+    
+]
+ router.get("/",(req,res)=>{
+    
+  res.send(usuarios);
+ })
+ router.post("/",(req,res)=>{
+ const nome=req.body.nome;
+ res.status(200).send({dados:nome})
+ });
 
 module.exports = router;
-
